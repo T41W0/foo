058c6de0e00b999687dada772c33ca4cfec4911d
techwolf001 <fawasrufai999@gmail.com>
2023-12-08 11:20:57 +0100

    techwolf001

--- foo_back/database/migrations/2023_12_08_101510_create_products_table.php
+++ foo_back/database/migrations/2023_12_08_101510_create_products_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->text('role');
-            $table->rememberToken();
+            $table->bigInteger('category');
+            $table->string('name');
+            $table->string('price');
+            $table->text('photo');
+            $table->string('discount');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
