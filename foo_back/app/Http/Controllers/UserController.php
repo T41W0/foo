8099c824eaeb07ba276ebbf825d34ca2fa1ec52e
wcodewiz <samuelclinton975@gmail.com>
2023-12-08 07:22:35 -0800

    wcodewiz

---
+++ foo_back/app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Utils\Errors\ForbiddenResponse;
+use App\Utils\Errors\SuccessResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required",
+        ]);
+        if ($validate->fails()) {
+            return new ForbiddenResponse($validate->messages()->all());
+        }
+        $query = User::where("email", "=", $request->email)->first();
+        if (!$query) {
+            return new ForbiddenResponse(["message" => "user does not exist"]);
+        }
+        if (!Hash::check($request->password, $query->password)) {
+            return new ForbiddenResponse(["message" => "invalid password"]);
+        }
+        //send login mail
+        auth()->login($query);
+        $token = $query->createToken($request->email . "" . env("APP_NAME") . "_token", ["user"])->plainTextToken;
+
+        return new SuccessResponse(["message" => "login success", "token" => $token]);
+
+    }
+    public function create(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required",
+        ]);
+        if ($validate->fails()) {
+            return new ForbiddenResponse($validate->messages()->all());
+        }
+        $query = User::where("email", "=", $request->email)->first();
+        if ($query) {
+            return new ForbiddenResponse(["message" => "user already exist"]);
+        }
+        $user = new User();
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = "user";
+        $user->save();
+        //send login mail
+        //   auth()->login($query);
+        $token = $user->createToken($request->email . "" . env("APP_NAME") . "_token", ["user"])->plainTextToken;
+
+        return new SuccessResponse(["message" => "registration success", "token" => $token]);
+
+
+    }
+    public function forgetPassword(Request $request)
+    {
+
+    }
+    public function changePassword(Request $request)
+    {
+
+    }
+    public function logout()
+    {
+        auth()->logout();
+        return new SuccessResponse(["message" => "logout success"]);
+    }
+}
--- foo_back/routes/api.php
+++ foo_back/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Utils\Errors\ForbiddenResponse;
+use App\Utils\Errors\SuccessResponse;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +20,129 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/login", function () {
+    return new ForbiddenResponse(["message" => "You must log in first!!"]);
+})->name("login");
+
+
+/**
+ * guest
+ * login
+ * create
+ * forgetPassword
+ *changePassword
+ * 
+ */
+Route::post("/account/login", [UserController::class, "login"]);
+Route::post("/create", [UserController::class, "create"]);
+Route::post("/forget/password", [UserController::class, "forgetPassword"]);
+Route::post("/change/password", [UserController::class, "changePassword"]);
+
+
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get("/auth", function () {
+        return new SuccessResponse(['message' => "logged in",]);
+    });
+
+    /**
+     * product
+     * get
+     */
+    Route::get("/product/get/user", [ProductController::class, "get"]);
+
+    /**
+     * category
+     * get
+     */
+    Route::get("/category/get/user", [CategoryController::class, "get"]);
+
+    /**
+     * Cart
+     * create
+     * get
+     */
+    Route::get("/cart/get/user", [CartController::class, "get"]);
+    Route::post("/cart/create", [CartController::class, "add"]);
+
+
+    /**
+     * order
+     * create
+     * get
+     */
+    Route::get("/order/get/user", [OrderController::class, "get"]);
+    Route::post("/order/create", [OrderController::class, "add"]);
+
+    Route::get("/category/fetch/{id}", [CategoryController::class, "fetch"]);
+
+
+    /**
+     * authenticated
+     *logout
+     */
+    Route::delete("/logout", [UserController::class, "logout"]);
+
+});
+
+
+Route::middleware(["isAdmin"])->group(function () {
+    /**
+     * product
+     * add
+     * delete
+     * update
+     * get
+     */
+    Route::get("/product/get", [ProductController::class, "get"]);
+    Route::get("/product/fetch/{id}", [ProductController::class, "fetch"]);
+    Route::post("/product/add", [ProductController::class, "add"]);
+    Route::post("/product/update/{id}", [ProductController::class, "update"]);
+    Route::delete("/product/delete/{id}", [ProductController::class, "delete"]);
+
+
+    /**
+     * category
+     * add
+     * update
+     * delete
+     * get
+     */
+    Route::get("/category/get", [CategoryController::class, "get"]);
+    Route::post("/category/add", [CategoryController::class, "add"]);
+    Route::post("/category/update/{id}", [CategoryController::class, "update"]);
+    Route::delete("/category/delete/{id}", [CategoryController::class, "delete"]);
+
+
+
+    /**
+     * Cart
+     * get
+     * delete
+     */
+    Route::get("/cart/get", [CartController::class, "get"]);
+    Route::get("/cart/fetch/{id}", [CartController::class, "fetch"]);
+    Route::delete("/cart/delete/{id}", [CartController::class, "delete"]);
+
+
+
+    /**
+     * order
+     * get
+     * delete
+     */
+
+    Route::get("/order/get", [OrderController::class, "get"]);
+    Route::get("/order/fetch/{id}", [OrderController::class, "fetch"]);
+    Route::delete("/order/delete/{id}", [OrderController::class, "delete"]);
+
+    /**
+     * user
+     * ban
+     * unban
+     */
+
+
 });
