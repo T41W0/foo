d2b95dc6d1f69a0b7ba211c9341d9c89498dd4db
techwolf001 <fawasrufai999@gmail.com>
2023-12-12 17:38:45 +0100

    techwolf001

--- foo_front/src/layouts/guestlayout.tsx
+++ foo_front/src/layouts/guestlayout.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import { AuthUseCase } from "../remotes/usecases/authusecase";
+import { FC, useEffect, } from "react";
+import { Outlet } from "react-router-dom";
 import { AccountState } from "../states/accountstate";
 import { useAuth } from "../hooks/guest/useAuth";
 
@@ -18,8 +17,11 @@ export const GuestLayout: FC = () => {
         return <span>loading...</span>
     }
 
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
     return !account.isLogin &&
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
         <AccountState.Provider value={{ account, setAccount }}> <div className="flex items-center justify-center h-screen px-4 py-2 my-0">
             <div className="w-3/6 rounded-md bg-violet-500 h-4/6">
                 <h1 className="flex items-center justify-center w-full py-2 my-4 font-sans text-4xl text-white h-3/6">Welcome to Our Store</h1>
--- foo_front/src/pages/guest/signup.tsx
+++ foo_front/src/pages/guest/signup.tsx
@@ -1,14 +1,23 @@
-import { FC } from "react";
-import { useRemoteRegister } from '../../remote/guest/allguestcalls';
+import { FC,useState,useContext } from "react";
 import { useForm } from "../../hooks/guest/useForm";
+import { SignupUseCase } from "../../remotes/usecases/signupusecase";
+import { AccountState } from "../../states/accountstate";
 
 export const Signup: FC = () => {
+    const [loading,setLoading]=useState(false);
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
     const [input, change] = useForm({
         initialState: {}
     });
-    const [login] = useRemoteRegister(input);
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    const { acccount, setAccount } = useContext(AccountState);
+    
+    if(loading){
+        return <span>creating account...</span>
+    }
 
     return <form className="flex flex-col justify-start w-full h-full gap-3 px-2 py-1 bg-white rounded-md">
         <h6>Signup</h6>
@@ -19,6 +28,20 @@ export const Signup: FC = () => {
         <button
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
-            onClick={login} className="px-4 py-2 font-sans text-white rounded-md bg-rose-600">Sign up</button>
+            onClick={(ev)=>{
+                setLoading(true);
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-ignore
+                SignupUseCase.execute(ev,input).then(value=>{
+                    const data = value.success;
+                    localStorage.setItem("token", data!.token);
+                    setAccount({ ...acccount, isLogin: true });
+                    setLoading(false);
+
+                }).catch(value=>{
+                    alert(value.message);
+                    setLoading(false);
+                });
+            }} className="px-4 py-2 font-sans text-white rounded-md bg-rose-600">Sign up</button>
     </form>
 }
